4a3945111bbdf71a139cb602f4eeb6910691b6da
Maurice Walker <mauricedw22@gmail.com>
2021-04-27 14:47:38 -0400

    Update db.js

--- web_app/config/db.js
+++ web_app/config/db.js
@@ -1,5 +1,5 @@
 
 module.exports = {
 
-   'url' : 'mongodb://localhost/infinid' // 'mongodb+srv://mauricedw22:dataMan@cluster1.slfse.mongodb.net/infinID?retryWrites=true&w=majority'
+   'url' : 'mongodb://localhost/infinid'
 };
